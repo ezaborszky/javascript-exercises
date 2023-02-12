f367b2d286bc4926f28ebc6d47f13fdcc4b06b9b
ezaborszky <124446749+ezaborszky@users.noreply.github.com>
2023-02-12 21:49:27 +0100

    solved exerise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(text,iteration) {
+   const textOut = [];
+   if (iteration < 0) {
+    return ("ERROR")
+   } else {
+    for (let i = 0; i < iteration; i++) {
+    textOut[textOut.length] = text;
+   }}
+   return textOut.join("");
 };
 
+repeatString("hey", 3)
+repeatString("hey", 10)
+repeatString("hey", 1)
+repeatString("hey", 0)
+repeatString("hey", -1)
+repeatString("hey",)
+repeatString("",)
 // Do not edit below this line
 module.exports = repeatString;
