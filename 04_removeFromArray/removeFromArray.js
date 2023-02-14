7933b89bb968049ee33e68bb2aaa82bbdc0e0a01
ezaborszky <124446749+ezaborszky@users.noreply.github.com>
2023-02-14 20:06:28 +0100

    solved exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
+    
+    numbersArray = arguments[0]
+    for (const x in arguments) {
+        if ((numbersArray.indexOf(arguments[x])>-1)){
+            //console.log(`The number ${arguments[x]} was found at index ${numbersArray.indexOf(arguments[x])}`);
+            numbersArray.splice(numbersArray.indexOf(arguments[x]),1);
+            
 
+        }
+        
+    }
+       
+return numbersArray  
 };
 
 // Do not edit below this line
